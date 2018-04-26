8c0b3dc1aaee652d93e775a33dd11fba71146ebf
kat284 <kat284@vt.edu>
2018-04-26 03:20:45 +0000

    major updates 2

--- server.py
+++ server.py
@@ -25,10 +25,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 """ Example of browsing for a service (in this case, HTTP) """
-import logging
 import socket
 import sys
-from time import sleep
 
 from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
 
@@ -39,7 +37,6 @@ from pymongo import MongoClient
 from flask_httpauth import HTTPBasicAuth
 
 from canvas_token import *
-import requests
 import urllib
 import json
 
@@ -48,12 +45,6 @@ import json
 #::::'/::::::::'/::::::::'/::::::::'/::::::::'/::::::::'/::::::::'/:::::#
 # `--'      `-.'      `--'      `--'      `--'      `--'      `.-'      #
 # ~~[Variables]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-from flask import Flask, request, jsonify, make_response, send_file
-import pymongo
-import requests
-from pymongo import MongoClient
-from flask_httpauth import HTTPBasicAuth
 
 address1 = None
 port1 = None
